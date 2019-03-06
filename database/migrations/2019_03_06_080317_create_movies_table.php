e58242f2502334048939049041b7c2c3886c9d14
hamdicatal <hamdicatal@outlook.com>
2019-03-06 11:53:24 +0300

    Migration'lar oluşturuldu

---
+++ database/migrations/2019_03_06_080317_create_movies_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateMoviesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('movies', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('name');
+            $table->date('release_date');
+            $table->unsignedInteger('genre_id');
+            $table->string('director');
+            $table->text('description');
+            $table->string('media');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('movies');
+    }
+}
---
+++ database/migrations/2019_03_06_084624_create_movie_langs_pivot_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateMovieLangsPivotTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('movie_langs', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->unsignedInteger('movie_id');
+            $table->unsignedInteger('cast_id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('movie_langs');
+    }
+}
