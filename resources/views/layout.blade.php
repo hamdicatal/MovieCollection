4c35e09c2f842fb544356512ad9629933b22635c
hamdicatal <hamdicatal@outlook.com>
2019-03-06 12:50:05 +0300

    Proje genelinde kullanılmak üzere temel layout ayarlandı.

---
+++ resources/views/layout.blade.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>Bootstrap Example</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+</head>
+<body>
+
+
+
+<div class="container">
+
+    @include('navbar')
+
+    <div class="card" style="margin-top: 20px">
+        <div class="card-body">
+            <h4 class="card-title">Home Page</h4>
+            <p class="card-text">Welcome to Movie Collection home page!</p>
+        </div>
+
+    </div>
+
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
+
+</body>
+</html>
---
+++ resources/views/navbar.blade.php
@@ -0,0 +1,19 @@
+<nav class="navbar navbar-expand-sm bg-dark navbar-dark" style="margin-top: 10px">
+        <ul class="navbar-nav">
+          <li class="nav-item active">
+            <a class="nav-link" href="#">Home</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="movies">Movies</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="genres">Genres</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="casts">Casts</a>
+            </li>
+            <li class="nav-item">
+            <a class="nav-link" href="Languages">Languages</a>
+            </li>
+        </ul>
+      </nav>
